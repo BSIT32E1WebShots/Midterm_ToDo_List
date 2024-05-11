8c9bad291db0c9a8d7b7fe67797cc99c15eb0bb5
CantonjosArman-BSIT32E1 <160833955+CantonjosArman-BSIT32E1@users.noreply.github.com>
2024-05-11 18:46:24 +0800

    Add Class (AppDbContext.cs, ToDoUsers.cs)

---
+++ ToDo.Domain/ToDoUsers.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToDo.Domain
+{
+    public class ToDo
+    {
+        public int ItemNumber { get; set; }
+
+        public int Id { get; set; }
+
+        public string Task { get; set; }
+
+        public string Date { get; set; }
+
+        public bool Done { get; set; } = false;
+    }
+}
--- ToDo.Domain/obj/Debug/net8.0/ToDo.Domain.AssemblyInfo.cs
+++ ToDo.Domain/obj/Debug/net8.0/ToDo.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDo.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f5b7e05b88a664aff5ba25efbbe11b820cd0eed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8ea04357ac21a371dc7658fb01ce9c5e614e5b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDo.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDo.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
